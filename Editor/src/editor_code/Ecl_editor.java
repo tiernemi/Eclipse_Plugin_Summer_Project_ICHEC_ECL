acbd00f35ed42ff3c46207f7a33fef321148cf45
Michael Tierney <tiernemi@tcd.ie>
2015-06-15 13:54:32 +0100

    Changed from jar to source

---
+++ Editor/src/editor_code/Ecl_editor.java
@@ -0,0 +1,16 @@
+// As barebones as you can get Text Editor //
+
+package editor_code;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class Ecl_editor extends TextEditor {
+	
+	public static final String ID = "ecl_editor.editor" ;
+	
+	public Ecl_editor()
+	{
+		;
+	}
+
+}
