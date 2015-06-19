72ce56e3ac94cd82925fbe124e4797ede8328f88
Michael Tierney <tiernemi@tcd.ie>
2015-06-19 10:23:41 +0100

    Tidied up and renamed files in Wordcount to make it more logical. Used the receiver object rather than the getCurrentSelection in property tester.

--- WordCount/src/wordcount/proptesters/EmptyFilePropertyTester.java
+++ WordCount/src/wordcount/proptesters/EmptyFilePropertyTester.java
@@ -7,15 +7,11 @@ import java.util.Scanner;
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.ui.PlatformUI;
 
 public class EmptyFilePropertyTester extends PropertyTester {
 
 	public static final String PROPERTY_NAMESPACE = "name.file.empty";
-	public static final String PROPERTY_IS_Ecl_Empty= "IsECLFileEmpty";
+	public static final String PROPERTY_IS_ECL_FILE_NOT_EMPTY= "IsECLFileNotEmpty";
 	
 	public EmptyFilePropertyTester() {
 		// TODO Auto-generated constructor stub
@@ -24,57 +20,46 @@ public class EmptyFilePropertyTester extends PropertyTester {
 	@Override
 	public boolean test(Object receiver, String property, Object[] args,
 			Object expectedValue) {		
-		if (PROPERTY_IS_Ecl_Empty.equals(property)) 
+		if (PROPERTY_IS_ECL_FILE_NOT_EMPTY.equals(property)) 
 		{
-			ISelection ecl_file_select = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getSelection() ;
-			
-			if (ecl_file_select instanceof TreeSelection)
+			if (receiver instanceof IFile)
 			{
-				TreeSelection tree_select = (TreeSelection) ecl_file_select ;
-				Object[] selection_list = tree_select.toArray() ;
-
-				for (int i=0 ; i<selection_list.length ; ++i)
-				{
-					if (selection_list[i] instanceof IFile)
+				IFile ecl_file = (IFile) receiver ;
+				try {
+					InputStream contents = ecl_file.getContents();
+					Scanner stream_scanner = new Scanner(contents,"UTF-8") ;
+							
+					String streamString = " " ;
+					if (stream_scanner.hasNext())
+						streamString = stream_scanner.useDelimiter("\\Z").next() ;
+							
+					for (int j=0 ; j<streamString.length() ; ++j)
 					{
-						IFile ecl_file = (IFile) Platform.getAdapterManager().getAdapter(selection_list[i], IFile.class) ;
-						try {
-							InputStream contents = ecl_file.getContents();
-							Scanner stream_scanner = new Scanner(contents,"UTF-8") ;
-							
-							String streamString = " " ;
-							if (stream_scanner.hasNext())
-								streamString = stream_scanner.useDelimiter("\\Z").next() ;
-							
-							for (int j=0 ; j<streamString.length() ; ++j)
+						if (Character.isWhitespace(streamString.charAt(j)) != true)
 							{
-								if (Character.isWhitespace(streamString.charAt(j)) != true)
-								{
-									stream_scanner.close();
-									contents.close();
-									return true ;
-								}
+								stream_scanner.close();
+								contents.close();
+								return true ;
 							}
-							stream_scanner.close();
-							contents.close();
-						} 
-						catch (CoreException e) 
-						{
-							e.printStackTrace();
-						} 
-						catch (IOException e) 
-						{
-							e.printStackTrace();
 						}
+						stream_scanner.close();
+						contents.close();
+						return false ;
+					} 
+					catch (CoreException e) 
+					{
+						e.printStackTrace();
+					} 
+					catch (IOException e) 
+					{
+						e.printStackTrace();
 					}
-				}
-				return false ;
 			}
-			else
-				return false ;
+			
+			return false ;
 		}
-		else 
-			return false;
+		else
+			return false ;
 	}
 
 }
